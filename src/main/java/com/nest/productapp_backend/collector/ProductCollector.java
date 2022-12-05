cc512bd8b1a3494e921e4c90b6c67c21f6d2e654
hami-r <mynameissaitama54@gmail.com>
2022-12-05 17:36:56 +0530

    text changed

--- src/main/java/com/nest/productapp_backend/collector/ProductCollector.java
+++ src/main/java/com/nest/productapp_backend/collector/ProductCollector.java
@@ -1,5 +1,6 @@
 package com.nest.productapp_backend.collector;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
